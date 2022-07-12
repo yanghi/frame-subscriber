d149345d5a4ceba3baab56ca525893a0d64646b6
yanghi <yanghi-x@outlook.com>
2022-07-12 10:04:33 +0800

    improve rollup config and ts config

--- rollup.config.js
+++ rollup.config.js
@@ -2,38 +2,59 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import ts from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel';
+import path from 'path'
 
-export default [{
-    input: './src/index.ts',
+let shouldDeclaration = true
+
+function makeConf(options, plugins = []) {
+
+    const tsPlugin = ts({
+        extensions: ['.ts'],
+        tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+        cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache'),
+        tsconfigOverride: {
+            compilerOptions: {
+                declaration: shouldDeclaration
+            },
+            exclude: ['tests']
+        }
+    })
+
+    shouldDeclaration = false
+
+    return {
+        input: './src/index.ts',
+        ...options,
+        output: {
+            format: 'esm',
+            file: './dist/frame-subscriber.esm.bundle.js'
+        },
+        plugins: [
+            tsPlugin,
+            nodeResolve(),
+            ...plugins
+        ]
+    }
+}
+
+export default [makeConf({
     output: {
         format: 'esm',
         file: './dist/frame-subscriber.esm.bundle.js'
-    },
-    plugins: [
-        ts({
-            extensions: ['.ts']
-        }),
-        nodeResolve()
-    ]
-}, {
-    input: './src/index.ts',
+    }
+}),
+makeConf({
     output: {
         format: 'esm',
         file: './dist/frame-subscriber.esm.js',
         plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })]
-    },
-    plugins: [
-        ts({
-            extensions: ['.ts']
-        }),
-        nodeResolve(),
-        terser({
-            module: false,
-            compress: {
-                ecma: 2015,
-                pure_getters: true
-            }
-        }),
-        babel({ babelHelpers: 'bundled' })
-    ]
-}]
+    }
+}, [terser({
+    module: false,
+    compress: {
+        ecma: 2015,
+        pure_getters: true
+    }
+}),
+babel({ babelHelpers: 'bundled' })])
+]
